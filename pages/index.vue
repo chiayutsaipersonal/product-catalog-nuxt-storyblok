f0446329daa0ee8e46e7aece534c4947cb2b6380
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-05-19 15:45:24 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         Gentry Way Co., Ltd.
       </h1>
       <h2 class="subtitle">
-        Product Catalog Test Site
+        SSR Product Catalog website test
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
@@ -21,39 +21,36 @@ import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
-  }
+    Logo,
+  },
 }
 </script>
 
 <style>
-.container
-{
+.container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
-.subtitle
-{
+.subtitle {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-.links
-{
+.links {
   padding-top: 15px;
 }
 </style>
